393ed1c76d709804f5f0e1549824b1a2fa57e840
Sibonji <70277546+Sibonji@users.noreply.github.com>
2021-04-13 17:14:35 +0300

    Update exti.c

--- exti.c
+++ exti.c
@@ -19,6 +19,8 @@
 #define POS2 LL_GPIO_PIN_10
 #define POS3 LL_GPIO_PIN_11
 
+//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\
+
 static void rcc_config()
 {
     LL_FLASH_SetLatency(LL_FLASH_LATENCY_1);
@@ -26,8 +28,7 @@ static void rcc_config()
     LL_RCC_HSI_Enable();
     while (LL_RCC_HSI_IsReady() != 1);
 
-    LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSI_DIV_2,
-                                LL_RCC_PLL_MUL_12);
+    LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSI_DIV_2, LL_RCC_PLL_MUL_12);
 
     LL_RCC_PLL_Enable();
     while (LL_RCC_PLL_IsReady() != 1);
@@ -37,9 +38,11 @@ static void rcc_config()
     while (LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL);
 
     LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_1);
-    
-    SystemCoreClock = 48000000;
+
+    long SystemCoreClock = 48000000;
 }
+
+//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\
 
 static void gpio_config(void)
 {
@@ -50,7 +53,7 @@ static void gpio_config(void)
     LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA);
     LL_GPIO_SetPinPull(GPIOA, LL_GPIO_PIN_1, LL_GPIO_PULL_DOWN);
     LL_GPIO_SetPinPull(GPIOA, LL_GPIO_PIN_0, LL_GPIO_PULL_DOWN);
-    
+      
     LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOB);
     LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_0, LL_GPIO_MODE_OUTPUT);
     LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_1, LL_GPIO_MODE_OUTPUT);
@@ -67,30 +70,15 @@ static void gpio_config(void)
     
     LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA);
     LL_GPIO_SetPinMode(GPIOC, LL_GPIO_PIN_0, LL_GPIO_MODE_INPUT);
+     
     return;
 }
 
-static void exti_config(void)
-{
-    LL_APB1_GRP2_EnableClock(LL_APB1_GRP2_PERIPH_SYSCFG);
-
-    LL_SYSCFG_SetEXTISource(LL_SYSCFG_EXTI_PORTA, LL_SYSCFG_EXTI_LINE1);
-    LL_SYSCFG_SetEXTISource(LL_SYSCFG_EXTI_PORTA, LL_SYSCFG_EXTI_LINE0);
-    LL_EXTI_EnableIT_0_31(LL_EXTI_LINE_1);
-    LL_EXTI_EnableIT_0_31(LL_EXTI_LINE_0);
-
-    LL_EXTI_EnableFallingTrig_0_31(LL_EXTI_LINE_1);
-    LL_EXTI_EnableRisingTrig_0_31(LL_EXTI_LINE_1);
-
-    LL_EXTI_EnableFallingTrig_0_31(LL_EXTI_LINE_0);
-    LL_EXTI_EnableRisingTrig_0_31(LL_EXTI_LINE_0);
-
-    NVIC_EnableIRQ(EXTI0_1_IRQn);
-    NVIC_SetPriority(EXTI0_1_IRQn, 0);
-}
+//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\
 
 int button_status, debouncer_clk = 0;
-int GPIOA_0_Button_Status()
+
+int A0_Button_Status()
 { 
     if (LL_GPIO_IsInputPinSet(GPIOA, LL_GPIO_PIN_0)) 
     {
@@ -113,60 +101,62 @@ int GPIOA_0_Button_Status()
     return button_status;
 }
 
-void dyn_display(uint16_t number, int digit_num)
-{
+//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\
 
+void dyn_display(uint16_t number, int pos_num)
+{
     static uint32_t mask = A | B | C | D | E | F | G | PD | POS0 | POS1 | POS2 | POS3;
 
-
     uint32_t decoder[16] = {
-    A | B | C | D | E | F,     //0
-    B | C,                     //1
-    A | B | D | E | G,         //2
-    A | B | C | D | G,         //3
-    B | C | F | G,             //4
-    A | C | D | F | G,         //5 
-    A | C | D | E | F | G,     //6
-    A | B | C,                 //7
-    A | B | C | D | E | F | G, //8
-    A | B | C | D | F | G,     //9
-    A | B | C | E | F | G,     //A
-    A | B | C | D | E | F | G, //B
-    A | D | E | F,             //C
-    A | B | C | D | E | F,     //D
-    A | D | E | F | G,         //E
-    A | E | F | G,             //F
-   
+        A | B | C | D | E | F,      //0
+        B | C,                      //1
+        A | B | D | E | G,          //2
+        A | B | C | D | G,          //3
+        B | C | F | G,              //4
+        A | C | D | F | G,          //5
+        A | C | D | E | F | G,      //6
+        A | B | C,                  //7
+        A | B | C | D | E | F | G,  //8
+        A | B | C | D | F | G,      //9
+        A | B | C | E | F | G,      //A
+        A | B | C | D | E | F | G,  //B
+        A | D | E | F,              //C
+        A | B | C | D | E | F,      //D
+        A | D | E | F | G,          //E
+        A | E | F | G,              //F
     };
 
     uint32_t out = 0;
 
-    uint32_t position[4] = {  
-           POS1 | POS2 | POS3,//first indicator
-    POS0 |        POS2 | POS3,//second indicator
-    POS0 | POS1 |        POS3,//third indicator
-    POS0 | POS1 | POS2,       //fourth indicator 
+    uint32_t position[4] = {    
+               POS1 | POS2 | POS3,//first indicator
+        POS0 |        POS2 | POS3,//second indicator
+        POS0 | POS1 |        POS3,//third indicator
+        POS0 | POS1 | POS2,       //fourth indicator 
     };
     
-    uint32_t num[4] = {      
-    decoder[number % 10],
-    decoder[(number % 100)   / 10],
-    decoder[(number % 1000)  / 100],
-    decoder[(number % 10000) / 1000],
+    uint32_t num[4] = {  
+        decoder[ number % 10],
+        decoder[(number % 100)   / 10],
+        decoder[(number % 1000)  / 100],
+        decoder[(number % 10000) / 1000],
     };
     
-    out = num[digit_num % 4] | position[digit_num % 4];
+    out = num[pos_num % 4] | position[pos_num % 4];
 
     uint32_t port_state = 0;
-
     port_state = LL_GPIO_ReadOutputPort(GPIOB);
     port_state = (port_state & ~mask) | out;
     LL_GPIO_WriteOutputPort(GPIOB, port_state);
 
-    digit_num = (digit_num + 1) % 4;
+    pos_num = (pos_num + 1) % 4;
 
     return;
 }
+
+//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\
+
+static int counter_top = 100;
 
 static void systick_config(void)
 {
@@ -176,37 +166,34 @@ static void systick_config(void)
     return;
 }
 
-static int counter_top = 1000;
-int digit_num = 0;
-int numdisp, newnumdisp = 0;
+//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\
 
+int pos_num = 0;
+int disp_num, newnum = 0;
+ 
 void SysTick_Handler(void)
-{
+{       
     static int counter = 0;
     counter = (counter + 1) % counter_top;
     
-    dyn_display(newnumdisp, digit_num);
-    digit_num ++;
+    dyn_display(newnum, pos_num);
+    pos_num++;
            
-    a0_button_status = GPIOA_0_Button_Status(); 
+    button_status = A0_Button_Status(); 
         
-    if(a0_button_status > 0)
-        newnumdisp = numdisp + 1;
-       
-    if(a0_button_status == 0)
-        numdisp = newnumdisp;       
-    
-    if (!counter)
-      LL_GPIO_TogglePin(GPIOC, LL_GPIO_PIN_8);
+    if      (button_status > 0)  newnum   = disp_num + 1;
+    else if (button_status == 0) disp_num = newnum;       
 }
+
+//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\
 
 int main(void)
 {
     rcc_config();
     gpio_config();
-    exti_config();
     systick_config();
 
+    
     while (1);
     return 0;
 }
